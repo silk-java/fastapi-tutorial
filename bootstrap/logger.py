5e6e23fdf1111bef6bee3404402e8701b26cc7b6
silk <java0904@think-land.com>
2023-12-04 17:04:54 +0800

    修改日志文件的大小

--- bootstrap/logger.py
+++ bootstrap/logger.py
@@ -10,7 +10,7 @@ if not os.path.exists(log_path):
 # 配置日志
 logger.add(
     os.path.join(log_path, "{time:YYYY-MM-DD}.log"),  # 日志文件路径，每天一个新文件
-    rotation="1 MB",  # 文件达到10MB时创建新的日志文件
+    rotation="100 MB",  # 文件达到10MB时创建新的日志文件
     retention="30 days",  # 保留最新30天的日志文件
     # compression="zip",  # 压缩历史日志文件
     level="INFO"  # 日志级别
